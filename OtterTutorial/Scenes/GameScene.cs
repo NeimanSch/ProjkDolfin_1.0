c8b9d8f0e9b56f5a960b233bf4c32568755f899a
NeimanSch <neiman@live.com>
2014-11-26 11:27:02 -0500

    Initial Commit
    Project starting point.

---
+++ OtterTutorial/Scenes/GameScene.cs
@@ -0,0 +1,112 @@
+﻿using Otter;
+
+using OtterTutorial.Entities;
+
+using System;
+using System.IO;
+
+namespace OtterTutorial.Scenes
+{
+    public class GameScene : Scene
+    {
+        public Music gameMusic = new Music(Assets.MUSIC_GAME);
+
+        // Our Tilemap's calculated width and height
+        public const int WIDTH = Global.GAME_WIDTH * 3;
+        public const int HEIGHT = Global.GAME_HEIGHT * 2;
+        public const float HALF_SCRENE_X = Global.GAME_WIDTH / 2;
+        public const float HALF_SCRENE_Y = Global.GAME_HEIGHT / 2;
+        public Tilemap tilemap = null;
+        public GridCollider grid = null;
+
+        // Our new constructor takes in the new J,I coordinates, and a Player object
+        public GameScene(Player player = null)
+            : base()
+        {
+            // If a Player object isn't passed in, start at the default x,y position of 100,100
+            if (player == null)
+            {
+                Global.player = new Player(100, 100);
+            }
+            else
+            {
+                Global.player = player;
+            }
+            // Create and load our Tilemap and GridCollider
+            tilemap = new Tilemap(Assets.TILESET, WIDTH, HEIGHT, Global.GRID_WIDTH, Global.GRID_HEIGHT);
+            grid = new GridCollider(WIDTH, HEIGHT, Global.GRID_WIDTH, Global.GRID_HEIGHT);
+
+            string mapToLoad = Assets.MAP_WORLD;
+            string solidsToLoad = Assets.MAP_SOLID;
+            LoadWorld(mapToLoad, solidsToLoad);
+            // Since we are constantly switching Scenes we need to do some checking,
+            // ensuring that the music doesn't get restarted.
+            // We should probably add an isPlaying boolean to the Music class. I will do this soon.
+            if (Global.gameMusic == null)
+            {
+                Global.gameMusic = new Music(Assets.MUSIC_GAME);
+                Global.gameMusic.Play();
+                Global.gameMusic.Volume = 0.40f;
+            }
+        }
+
+        // We now add our Entities and Graphics once the Scene has been switched to
+        public override void Begin()
+        {
+            Entity gridEntity = new Entity(0, 0, null, grid);
+            Add(gridEntity);
+            AddGraphic(tilemap);
+            // Ensure that the player is not null
+            if (Global.player != null)
+            {
+                Add(Global.player);
+                // Never should be paused once transitioning is complete
+                Global.paused = false;
+            }
+            Add(Global.camShaker);
+            // This is rather crude, as we re-add the Enemy every time we switch screens
+            // A good task beyond these tutorials would be ensuring that non-player
+            // Entities retain their state upon switching screens
+            Add(new Enemy(500, 400));
+        }
+
+        private void LoadWorld(string map, string solids)
+        {
+            // Get our CSV map in string format and load it via our tilemap
+            string newMap = CSVToString(map);
+            tilemap.LoadCSV(newMap);
+
+            // Get our csv solid map and load it into our GridCollider
+            string newSolids = CSVToString(solids);
+            grid.LoadCSV(newSolids);
+        }
+
+        // Add this method to your GameScene.cs class
+        private static string CSVToString(string csvMap)
+        {
+            string ourMap = "";
+            using (var reader = new StreamReader(csvMap))
+            {
+                // Read each line, adding a line-break to the end of each
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    ourMap += line;
+                    ourMap += "\n";
+                }
+            }
+
+            return ourMap;
+        }
+
+        public override void Update()
+        {
+            if (Global.paused)
+            {
+                return;
+            }
+            this.CameraX = Global.player.X - HALF_SCRENE_X;
+            this.CameraY = Global.player.Y - HALF_SCRENE_Y;
+        }
+    }
+}
---
+++ OtterTutorial/Scenes/TitleScene.cs
@@ -0,0 +1,77 @@
+﻿using Otter;
+
+using OtterTutorial;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OtterTutorial.Scenes
+{
+    public class TitleScene : Scene
+    {
+        // Create a new Image object, referencing the Otter image in our Assets folder
+        public Image titleImage = new Image(Assets.TITLE_IMG);
+
+        public Text titleText = new Text("Otter Tutorial", Assets.FONT_PANIC, 84);
+        public Text enterText = new Text("Press Enter", Assets.FONT_PANIC, 40);
+
+        public const float TIMER_BLINK = 25f;
+        public float blinkTimer = 0;
+
+        // Create a new, looping sound object, with our MUSIC_TITLE as its source
+        public Music titleSong = new Music(Assets.MUSIC_TITLE, true);
+
+        public TitleScene()
+        {
+            // Center the title picture 
+            titleImage.CenterOrigin();
+            titleImage.X = Global.TUTORIAL.HalfWidth;
+            titleImage.Y = 1000; // When tweening something in, make sure it is actually off the screen first
+            this.AddGraphic(titleImage);
+
+            // Otter utilizes the C# Tweening library called Glide
+            // More info can be found here: http://www.reddit.com/r/gamedev/comments/1fabdh/
+            // Below, we tween the titleImage to its new Y position, in .30 seconds. Our Easing function utilized is Ease.BackOut
+            Glide.GlideManagerImpl.Tweener.Tween(titleImage, new { Y = 250 }, 30f, 0f).Ease(Ease.BackOut);
+
+            // Set the text's outline color to the 
+            // hex color #7FA8D2 (Otter2d.com Blue)
+            titleText.OutlineColor = new Otter.Color("7FA8D2");
+            titleText.OutlineThickness = 3; // Set the outline thickness to 3 pixels
+            titleText.CenterOrigin();
+            titleText.X = Global.TUTORIAL.HalfWidth;
+            titleText.Y = 25;
+            this.AddGraphic(titleText);
+
+            enterText.OutlineColor = new Otter.Color("7FA8D2");
+            enterText.OutlineThickness = 2;
+            enterText.CenterOrigin();
+            enterText.X = Global.TUTORIAL.HalfWidth;
+            enterText.Y = 450;
+            this.AddGraphic(enterText);
+
+            titleSong.Play();
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            blinkTimer++;
+            if (blinkTimer >= TIMER_BLINK)
+            {
+                enterText.Visible = !enterText.Visible;
+                blinkTimer = 0;
+            }
+
+            if (Global.PlayerSession.Controller.Start.Pressed)
+            {
+                titleSong.Stop();
+
+                Global.TUTORIAL.RemoveScene();
+                Global.TUTORIAL.AddScene(new GameScene());
+            }
+        }
+    }
+}
